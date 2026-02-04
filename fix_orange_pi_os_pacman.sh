12e002cdc25865696f290334b71aaee18965eee1
SUSCERSAL <159478767+suscersal@users.noreply.github.com>
2026-02-04 20:22:38 +0300

    Add files via upload

---
+++ fix_orange_pi_os_pacman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_lang=$LANG
+echo "Текущий язык системы: $current_lang"
+
+if [[ $LANG == ru* ]]; then
+    echo "Добро пожаловать в скрипт для фикса ошибки полного обновления системы. Я создал этот скрипт после того как я поставил на свой 'opi zero 3' 'opi os' и не мог полностью обновить систему.Приятного использования!"
+else
+    echo "Welcome to the script for fixing a full system update error. I created this script after I installed opi os on my opi zero 3 and couldn't fully update the system."
+fi
+
+sudo pacman -R libcamera, libcamera-ipa
+
+sudo pacman -Syu
